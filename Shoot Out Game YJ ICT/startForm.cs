ec3c802a623ce26de5c0bf00cb13ec2e83ad5eeb
YJ\iooi5 <iooi567@naver.com>
2025-10-07 18:07:17 +0900

    1p 2p 수정

--- Shoot Out Game YJ ICT/startForm.cs
+++ Shoot Out Game YJ ICT/startForm.cs
@@ -4,20 +4,18 @@ using System.Windows.Forms;
 using System.Net.Sockets;
 using System.Text;
 
-
 namespace Shoot_Out_Game_YJ_ICT
 {
     public partial class startForm : Form
     {
-        bool coinInserted = false;
-
         TcpClient client;
         NetworkStream stream;
+        bool coinInserted = false;
 
         public startForm()
         {
             InitializeComponent();
-            this.KeyPreview = true; 
+            this.KeyPreview = true;
             this.KeyDown += StartForm_KeyDown;
 
             lblTitle.TextAlign = ContentAlignment.MiddleCenter;
@@ -27,45 +25,52 @@ namespace Shoot_Out_Game_YJ_ICT
 
         private void StartForm_KeyDown(object sender, KeyEventArgs e)
         {
-
+            // 스페이스바를 눌러 서버 연결 시도
             if (!coinInserted && e.KeyCode == Keys.Space)
             {
                 try
                 {
                     client = new TcpClient();
-                    client.Connect("127.0.0.1", 9000);
+                    client.Connect("211.185.150.248", 9000);
                     stream = client.GetStream();
 
-                    MessageBox.Show("서버 연결 성공");
+                    byte[] buffer = new byte[1024];
+                    int bytes = stream.Read(buffer, 0, buffer.Length);
+                    string msg = Encoding.UTF8.GetString(buffer, 0, bytes);
+
+                    // 서버 응답 처리
+                    if (msg.StartsWith("REJECT"))
+                    {
+                        MessageBox.Show(msg.Split(',')[1]); // "게임이 이미 시작되었습니다."
+                        client.Close();
+                        return;
+                    }
+                    else if (msg.StartsWith("ROLE"))
+                    {
+                        int playerNum = int.Parse(msg.Split(',')[1]);
+                        MessageBox.Show($"{playerNum}P로 접속되었습니다!");
+
+                        bool isPlayer1 = (playerNum == 1);
+                        coinInserted = true;
+
+                        // 즉시 게임 폼으로 이동
+                        Form1 game = new Form1(client, stream, isPlayer1);
+                        game.Show();
+                        this.Hide();
+                    }
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     MessageBox.Show("서버 연결 실패 : " + ex.Message);
                     return;
                 }
-                coinInserted = true;
-                lblInsertCoin.Visible = true;
-                lblSelect.Visible = true;
             }
 
-            else if (coinInserted && e.KeyCode == Keys.D1)
-            {
-                Form1 game = new Form1(client, stream, true); // 1P
-                game.Show();
-                this.Hide();
-            }
-
-            else if (coinInserted && e.KeyCode == Keys.D2)
-            {
-                Form1 game = new Form1(client, stream, false); // 2P
-                game.Show();
-                this.Hide();
-            }
         }
 
         private void startForm_Load(object sender, EventArgs e)
         {
-            
+
         }
 
     }
