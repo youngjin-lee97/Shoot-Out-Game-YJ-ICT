5fa51067310f116d2dcc7d24147805bb569d3582
youngjin-lee97 <iooi567@naver.com>
2025-10-13 15:41:59 +0900

    코드 리뷰 -- 설명 주석 추가

--- Shoot Out Game YJ ICT/Form1.Designer.cs
+++ Shoot Out Game YJ ICT/Form1.Designer.cs
@@ -49,9 +49,9 @@
             this.txtAmmo.AutoSize = true;
             this.txtAmmo.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.txtAmmo.ForeColor = System.Drawing.Color.White;
-            this.txtAmmo.Location = new System.Drawing.Point(20, 27);
+            this.txtAmmo.Location = new System.Drawing.Point(18, 22);
             this.txtAmmo.Name = "txtAmmo";
-            this.txtAmmo.Size = new System.Drawing.Size(100, 23);
+            this.txtAmmo.Size = new System.Drawing.Size(91, 19);
             this.txtAmmo.TabIndex = 0;
             this.txtAmmo.Text = "Ammo: 0";
             // 
@@ -60,9 +60,9 @@
             this.txtScore.AutoSize = true;
             this.txtScore.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.txtScore.ForeColor = System.Drawing.Color.White;
-            this.txtScore.Location = new System.Drawing.Point(131, 27);
+            this.txtScore.Location = new System.Drawing.Point(115, 22);
             this.txtScore.Name = "txtScore";
-            this.txtScore.Size = new System.Drawing.Size(76, 23);
+            this.txtScore.Size = new System.Drawing.Size(69, 19);
             this.txtScore.TabIndex = 0;
             this.txtScore.Text = "Kills: 0";
             // 
@@ -71,17 +71,18 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(217, 27);
+            this.label2.Location = new System.Drawing.Point(190, 22);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 23);
+            this.label2.Size = new System.Drawing.Size(71, 19);
             this.label2.TabIndex = 0;
             this.label2.Text = "Health:";
             // 
             // healthBar
             // 
-            this.healthBar.Location = new System.Drawing.Point(301, 27);
+            this.healthBar.Location = new System.Drawing.Point(263, 22);
+            this.healthBar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.healthBar.Name = "healthBar";
-            this.healthBar.Size = new System.Drawing.Size(217, 23);
+            this.healthBar.Size = new System.Drawing.Size(190, 18);
             this.healthBar.TabIndex = 1;
             this.healthBar.Value = 100;
             // 
@@ -94,7 +95,8 @@
             // player
             // 
             this.player.Image = global::Shoot_Out_Game_YJ_ICT.Properties.Resources.up;
-            this.player.Location = new System.Drawing.Point(510, 941);
+            this.player.Location = new System.Drawing.Point(446, 753);
+            this.player.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.player.Name = "player";
             this.player.Size = new System.Drawing.Size(71, 100);
             this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -106,18 +108,19 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("굴림", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(18, 9);
+            this.label1.Location = new System.Drawing.Point(16, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.Size = new System.Drawing.Size(53, 11);
             this.label1.TabIndex = 0;
             this.label1.Text = "1player";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // healthBar2
             // 
-            this.healthBar2.Location = new System.Drawing.Point(838, 27);
+            this.healthBar2.Location = new System.Drawing.Point(733, 22);
+            this.healthBar2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.healthBar2.Name = "healthBar2";
-            this.healthBar2.Size = new System.Drawing.Size(217, 23);
+            this.healthBar2.Size = new System.Drawing.Size(190, 18);
             this.healthBar2.TabIndex = 7;
             this.healthBar2.Value = 100;
             // 
@@ -126,9 +129,9 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(754, 27);
+            this.label3.Location = new System.Drawing.Point(660, 22);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(80, 23);
+            this.label3.Size = new System.Drawing.Size(71, 19);
             this.label3.TabIndex = 3;
             this.label3.Text = "Health:";
             // 
@@ -137,9 +140,9 @@
             this.txtScore2.AutoSize = true;
             this.txtScore2.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.txtScore2.ForeColor = System.Drawing.Color.White;
-            this.txtScore2.Location = new System.Drawing.Point(668, 27);
+            this.txtScore2.Location = new System.Drawing.Point(584, 22);
             this.txtScore2.Name = "txtScore2";
-            this.txtScore2.Size = new System.Drawing.Size(76, 23);
+            this.txtScore2.Size = new System.Drawing.Size(69, 19);
             this.txtScore2.TabIndex = 4;
             this.txtScore2.Text = "Kills: 0";
             // 
@@ -148,9 +151,9 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("굴림", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(555, 9);
+            this.label5.Location = new System.Drawing.Point(486, 7);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.Size = new System.Drawing.Size(53, 11);
             this.label5.TabIndex = 5;
             this.label5.Text = "2player";
             // 
@@ -159,18 +162,19 @@
             this.txtAmmo2.AutoSize = true;
             this.txtAmmo2.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.txtAmmo2.ForeColor = System.Drawing.Color.White;
-            this.txtAmmo2.Location = new System.Drawing.Point(557, 27);
+            this.txtAmmo2.Location = new System.Drawing.Point(487, 22);
             this.txtAmmo2.Name = "txtAmmo2";
-            this.txtAmmo2.Size = new System.Drawing.Size(100, 23);
+            this.txtAmmo2.Size = new System.Drawing.Size(91, 19);
             this.txtAmmo2.TabIndex = 6;
             this.txtAmmo2.Text = "Ammo: 0";
+            this.txtAmmo2.Click += new System.EventHandler(this.txtAmmo2_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(1082, 1053);
+            this.ClientSize = new System.Drawing.Size(947, 842);
             this.Controls.Add(this.healthBar2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtScore2);
@@ -182,8 +186,10 @@
             this.Controls.Add(this.txtScore);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtAmmo);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Zombie Shootout Game YJ ICT";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
             ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
--- Shoot Out Game YJ ICT/Form1.cs
+++ Shoot Out Game YJ ICT/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// 표준 아이브러리, WinForms, 네트워킹, 스레드 등 필요한 네임스페이스를 불러옴
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,37 +12,45 @@ using System.Xml;
 using System.Net.Sockets;
 using System.Threading;
 
+
+// WinForms 폼 클래스이다.
 namespace Shoot_Out_Game_YJ_ICT
 {
     public partial class Form1 : Form
     {
-        bool goLeft, goRight, goUp, goDown, gameOver;
-        string facing = "up";
-        int playerHealth = 100;
-        int speed = 10;
-        int ammo = 10;
-        int score;
-        int zombieSpeed = 3;
-        Random randNum = new Random();
+        // 상태 변수
+        bool goLeft, goRight, goUp, goDown, gameOver; // 키 입력 지속 상태(눌러있는 동안 true)
+        string facing = "up"; // facing 플레이어가 바라보는 방향(총알 방향 및 네트워크 전송에 사용)
+        int playerHealth = 100; // 체력
+        int speed = 10; // 이동속도
+        int ammo = 10; // 탄약
+        int score; // 점수
+        int zombieSpeed = 3; // 좀비속도
+        Random randNum = new Random(); // 드랍/스폰 위치 난수
 
-        List<PictureBox> zombiesList = new List<PictureBox>();
+        List<PictureBox> zombiesList = new List<PictureBox>(); // 생성된 좀비들을 추적
 
-        TcpClient client;
-        NetworkStream stream;
+        // 서버와의 TCP 연결 및 수신 스레드
+        TcpClient client; // TCP 연결을 관리하는 객체
+        NetworkStream stream; // Client에서 데이터를 주고받을 수 있는 실제 전송 채널
         Thread recvThread;
 
-        PictureBox otherPlayer = new PictureBox();
+        PictureBox otherPlayer = new PictureBox(); // 상대 플레이어를 그릴 pictureBox (멀티플레이 표시용)
 
 
         public Form1(TcpClient c, NetworkStream s, bool isPlayer1)
         {
-            InitializeComponent();
-            this.Paint += new PaintEventHandler(Form1_Paint);
-            RestartGame();
+            // 처음 Form을 만들때 자동으로 생성되는 것이며, this.~ 이렇게 객체를 만들고
+            // 객체를 만들고 속성을 설정하고 이벤트를 연결하는 작업을 전부 담당한다.
+            InitializeComponent(); // Form1.Designer.cs
+            this.Paint += new PaintEventHandler(Form1_Paint); // UI프레임을 그리는 핸들러 추가
+            RestartGame(); // 게임 상태 리셋
+
+
 
             client = c;
             stream = s;
-
+            // 플레이어 1,2 시작 위치 설정
             if (isPlayer1)
             {
                 player.Left = 100;
@@ -53,7 +62,7 @@ namespace Shoot_Out_Game_YJ_ICT
                 player.Top = 300;
             }
 
-            otherPlayer = null;
+            otherPlayer = null; // 아직 상대가 안그려짐
 
             // 서버 수신 스레드 시작
             recvThread = new Thread(ReceiveData);
@@ -63,33 +72,45 @@ namespace Shoot_Out_Game_YJ_ICT
 
         private void ReceiveData()
         {
-            try
+            try // 네트워크 I/O 중 예외(연결 끊김 등)가 나올 수 있으니 예외 처리 블록 시작
             {
-                byte[] buffer = new byte[1024];
-                while (true)
+                byte[] buffer = new byte[1024]; // 수신 바이트를 임시로 담을 1KB 버퍼
+                while (true) // 무한 루프 ( 연결 종료 / 예외가 나면 빠져나감)
                 {
+                    // 반환값 bytes = 실제로 읽힌 바이트 수. 데이터가 올떄까지 대기
                     int bytes = stream.Read(buffer, 0, buffer.Length);
-                    if (bytes == 0) break;
+                    if (bytes == 0) break; // 0 이면 원격이 정상 종료 --> 루프ㅡ 중단
 
+
+                    // 방금 읽은 바이트를 UTF-8문자열로 변환
                     string msg = Encoding.UTF8.GetString(buffer, 0, bytes);
-                    string[] data = msg.Split(',');
+                    string[] data = msg.Split(','); // 받은 문자열을 쉼표로 분리
 
-                    if (data[0] == "POS" && data.Length == 4)
+                    if (data[0] == "POS" && data.Length == 4) // 4개의 문자열을 받는다면
                     {
-                        int x = int.Parse(data[1]);
-                        int y = int.Parse(data[2]);
-                        string dir = data[3];
+                        int x = int.Parse(data[1]); // pos 다음 문자열부터 1
+                        int y = int.Parse(data[2]); // 2
+                        string dir = data[3]; // 3
 
+
+
+
+                        // UI 스레드에서 컨트롤을 건드리기 위해서 Invoke를 사용한다.
+                        // WinForms는 UI 스레드가 아닌 곳에서 컨트롤 수정 금지
                         this.Invoke(new Action(() =>
+
                         {
-                            if (otherPlayer == null)
+                            if (otherPlayer == null) // 상대 플레이어가 없으면 동적 생성해서 폼에 추가
                             {
                                 otherPlayer = new PictureBox();
                                 otherPlayer.SizeMode = PictureBoxSizeMode.AutoSize;
                                 otherPlayer.Tag = "player2";
-                                this.Controls.Add(otherPlayer);
+                                this.Controls.Add(otherPlayer); // otherplayer 라는 PictureBox를 이폼에 올려서 화면에 보이게 했다
                             }
 
+
+                            // 상대 플레이어의 좌표이다.
+                            // 받은 방향 문자열에 맞춰 스프라이트 이미지 갱신
                             otherPlayer.Left = x;
                             otherPlayer.Top = y;
 
@@ -116,17 +137,23 @@ namespace Shoot_Out_Game_YJ_ICT
 
         private void SendPosition()
         {
+
+            // TCP 연결이 없거나 끊어졌을 떄는 함수 종료
             if (client == null || !client.Connected) return;
 
+            // 보낼 데이터를 문자열로 조합
             string msg = $"POS,{player.Left},{player.Top},{facing}";
             byte[] data = Encoding.UTF8.GetBytes(msg);
             try
             {
+                // 데이터를 서버에 보냄 ( stream 연결통로 )
                 stream.Write(data, 0, data.Length);
             }
             catch { }
         }
 
+
+        // UI 프레임 ---> 상단에 네모박스 UI player1,2 네모 박스임
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             using (Pen borderpen = new Pen(Color.White, 2))
@@ -143,8 +170,12 @@ namespace Shoot_Out_Game_YJ_ICT
         }
 
 
+
         private void MainTimerEvent(object sender, EventArgs e)
         {
+            // 체력이 1 이하면 사망처리
+            // 게임 오버 처리 되고
+            // 상대에게 dead 전송
             if(playerHealth > 1)
             {
                 healthBar.Value = playerHealth;
@@ -159,9 +190,13 @@ namespace Shoot_Out_Game_YJ_ICT
                 
             }
 
+            // UI 텍스트 갱신
             txtAmmo.Text = "Ammo: " + ammo;
             txtScore.Text = "kills: " + score;
 
+
+
+            // 이동 처리 ( 벽 충동 포함)
             if(goLeft == true && player.Left > 0)
             {
                 player.Left -= speed;
@@ -183,26 +218,33 @@ namespace Shoot_Out_Game_YJ_ICT
                 SendPosition();
             }
 
+
+            // this(현재 Form1) 에 있는것을 검사
             foreach (Control x in this.Controls)
             {
+                // 탄약상자만 골라 처리하는 로직
                 if(x is PictureBox && (string)x.Tag == "ammo")
                 {
+                    // x(ammo) 와 player 두 사각형이 겹치면 true
                     if(player.Bounds.IntersectsWith(x.Bounds))
                     {
-                        this.Controls.Remove(x);
-                        ((PictureBox)x).Dispose();
-                        ammo += 5;
+                        this.Controls.Remove(x); // 겹치면 x 가 사라짐
+                        ((PictureBox)x).Dispose(); // 실제 리소스 해제
+                        ammo += 5; // 총알 갯수 추가
                     }
                 }
 
-
+                // zombie 만 골라 처리하는 로직
                 if (x is PictureBox && (string) x.Tag == "zombie")
                 {
+                    //좀비와 플레이어 두 사각형이 겹치면 true
                     if(player.Bounds.IntersectsWith(x.Bounds))
                     {
+                        // 체력 -1
                         playerHealth -= 1;
                     }
 
+                    // 좀비 이동//플레이어 데미지
                     if (x.Left > player.Left)
                     {
                         x.Left -= zombieSpeed;
@@ -226,22 +268,29 @@ namespace Shoot_Out_Game_YJ_ICT
                 }
 
 
+
                 foreach ( Control j in this.Controls )
                 {
+                    // 총알과 좀비만 따로 처리하는 로직
                     if (j is PictureBox && (string)j.Tag == "bullet" && x is PictureBox && (string)x.Tag == "zombie")
                     {
+
+                        // 좀비와 총알 두 사각형이 겹쳤을 때 true
                         if(x.Bounds.IntersectsWith(j.Bounds))
                         {
+
+                            // 점수 +
                             score++;
 
-                            this.Controls.Remove(j);
-                            ((PictureBox)j).Dispose();
-                            this.Controls.Remove(x);
-                            ((PictureBox)x).Dispose();
-                            zombiesList.Remove(((PictureBox)x));
-                            MakeZombies();
+
+                            this.Controls.Remove(j); // 총알 제거
+                            ((PictureBox)j).Dispose(); // 총알 리소스 해제
+                            this.Controls.Remove(x); // 좀비 제거
+                            ((PictureBox)x).Dispose(); // 좀비 리소스 해제
+                            zombiesList.Remove(((PictureBox)x));// 리스트에서 해당 좀비 객체도 제거
+                            MakeZombies();// 새로운 좀비 생성
                             
-                            if (randNum.Next(0,5) == 0)
+                            if (randNum.Next(0,5) == 0) // 20% 확률 체력 드랍
                             {
                                 DropHeart();
                             }
@@ -250,17 +299,20 @@ namespace Shoot_Out_Game_YJ_ICT
                     }
                 }
 
+                
                 foreach ( Control y in this.Controls )
                 {
+                    // 하트만 처리하는 로직
                     if(y is PictureBox && (string)y.Tag == "heart")
                     {
+                        // 플레이어와 하트 두 사각형이 만났을 떄 true
                         if(player.Bounds.IntersectsWith(y.Bounds))
                         {
-                            this.Controls.Remove(y);
-                            ((PictureBox)y).Dispose();
+                            this.Controls.Remove(y);// 하트 제거
+                            ((PictureBox)y).Dispose(); // 하트 리소스 해제
 
-                            playerHealth += 20;
-                            if (playerHealth > 100) playerHealth = 100;
+                            playerHealth += 20; // 플레이어 체력 +20
+                            if (playerHealth > 100) playerHealth = 100; // 100 이상일때는 그대로 100
                         }
                     }
                 }
@@ -270,46 +322,55 @@ namespace Shoot_Out_Game_YJ_ICT
             }
         }
 
+
+        // 키 눌렀을때
         private void KeyIsDown(object sender, KeyEventArgs e)
         {
+            // 게임이 끝났으면 입력 무시
             if(gameOver == true)
             {
                 return;
             }
 
+            // 왼쪽방향키 눌렀을때
             if (e.KeyCode == Keys.Left)
             {
                 goLeft = true;
                 facing = "left";
                 player.Image = Properties.Resources.left;
-                SendPosition();
+                SendPosition();// 즉시 서버에 방향/위치 전송
             }
 
+            // 오른쪽방향키 눌렀을때
             if (e.KeyCode == Keys.Right)
             {
                 goRight = true;
                 facing = "right";
                 player.Image = Properties.Resources.right;
-                SendPosition();
+                SendPosition();// 즉시 서버에 방향/위치 전송
             }
 
+            // 위쪽 방향키 눌렀을때
             if (e.KeyCode == Keys.Up)
             {
                 goUp = true;
                 facing = "up";
                 player.Image = Properties.Resources.up;
-                SendPosition();
+                SendPosition();// 즉시 서버에 방향/위치 전송
             }
 
+            // 아래 방향키 눌렀을때
             if (e.KeyCode == Keys.Down)
             {
                 goDown = true;
                 facing = "down";
                 player.Image = Properties.Resources.down;
-                SendPosition();
+                SendPosition();// 즉시 서버에 방향/위치 전송
             }
         }
 
+
+        // 누른 키를 뗸 순간 로직
         private void KeyIsUp(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Left)
@@ -332,6 +393,8 @@ namespace Shoot_Out_Game_YJ_ICT
                 goDown = false;
             }
 
+
+            // 스페이스바 눌렀다 똈을 경우 총알 발사
             if (e.KeyCode == Keys.Space && ammo > 0 && gameOver == false)
             {
                 ammo--;
@@ -343,19 +406,23 @@ namespace Shoot_Out_Game_YJ_ICT
                 }
             }    
 
+            // 엔터 누르고 뗐을 경우 재시작
             if(e.KeyCode == Keys.Enter && gameOver ==true)
             {
                 RestartGame();
             }
         }
 
+
+        // 총알 생성 로직
         private void ShootBullet(string direction)
         {
-            Bullet shootBullet = new Bullet();
-            shootBullet.direction = direction;
+            Bullet shootBullet = new Bullet(); // 총알 객체 생성
+            shootBullet.direction = direction; // 어떤 방향으로 나가야 하는지 정보 저장
+            // 플레이어 중앙에서 나가게 설정
             shootBullet.bulletLeft = player.Left + (player.Width / 2);
             shootBullet.bulletTop = player.Top + (player.Height / 2);
-            shootBullet.MakeBullet(this);
+            shootBullet.MakeBullet(this);// 폼위에 표시
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -363,6 +430,17 @@ namespace Shoot_Out_Game_YJ_ICT
 
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAmmo2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // 좀비 생성 로직
         private void MakeZombies()
         {
             PictureBox zombie = new PictureBox();
@@ -377,6 +455,8 @@ namespace Shoot_Out_Game_YJ_ICT
             player.BringToFront();
         }
 
+
+        // 하트 트랍 로직
         private void DropHeart()
         {
             PictureBox heart = new PictureBox();
@@ -392,7 +472,7 @@ namespace Shoot_Out_Game_YJ_ICT
             player.BringToFront();
         }
 
-
+        // Ammo 드랍 로직
         private void DropAmmo()
         {
             PictureBox ammo = new PictureBox();
@@ -408,6 +488,7 @@ namespace Shoot_Out_Game_YJ_ICT
         }
         
 
+        // 게임 리셋
         private void RestartGame()
         {
             player.Image = Properties.Resources.up;
